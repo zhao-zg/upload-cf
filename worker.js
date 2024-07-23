23f611f025cbbff39ae71319525950dca5c3bfb4
zhao-zg <61549925+zhao-zg@users.noreply.github.com>
2024-07-23 16:19:10 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,14 +2,10 @@ addEventListener('fetch', event => {
   const request = event.request;
   const url = new URL(request.url);
   // 处理上传请求
-  //if (url.pathname == '/upload') {
-  //  return handleUpload(request);
-  //}
-
-  // 处理文件下载请求
-  if (url.pathname.startsWith('/download/')) {
-    return handleDownload(request);
+  if (url.pathname === '/upload' && request.method === 'POST') {
+    return handleUpload(request);
   }
+
   // 处理静态文件请求
   if (url.pathname.startsWith('/')) {
     return event.respondWith(fetch(request));
@@ -17,7 +13,7 @@ addEventListener('fetch', event => {
   // 处理其他请求
   return new Response('Invalid request', { status: 400 });
 });
-const handleUpload = async (request) => {
+async function handleUpload(request) {
   const formData = await request.formData();
   const file = formData.get('file');
 
@@ -50,23 +46,3 @@ const handleUpload = async (request) => {
   }
 }
 
-const handleDownload = async (request) => {
-  const url = new URL(request.url);
-  const fileName = url.pathname.slice('/download/'.length);
-
-  // 获取 R2 存储桶信息
-  const bucketName = 'sg-apk'; // 替换为你的 R2 存储桶名称
-  const bucket = R2.getBucket(bucketName);
-
-  // 从 R2 下载文件
-  const object = await bucket.get(fileName);
-  const body = object.body;
-
-  // 返回下载响应
-  return new Response(body, {
-    headers: {
-      'Content-Type': object.contentType,
-      'Content-Disposition': `attachment; filename="${fileName}"`
-    }
-  });
-}
