66df17e053244a400a86de49fd677483cd3a2a2c
zhao-zg <61549925+zhao-zg@users.noreply.github.com>
2024-07-23 11:48:11 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,9 @@
 addEventListener('fetch', event => {
   const request = event.request;
   const url = new URL(request.url);
-  return new Response('File uploaded successfully', { status: 200 });
   // 处理上传请求
   if (url.pathname == '/upload') {
-    return new Response('File uploaded successfully', { status: 200 });
-   // return handleUpload(request);
+    return handleUpload(request);
   }
 
   // 处理文件下载请求
